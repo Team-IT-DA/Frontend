e5cdee4bc7bd363f15572852a04b145bfb625450
Jungwon Lee <leejungwon96@gmail.com>
2021-07-27 23:38:35 +0900

    feat: product card 세로 스타일 틀 잡기

--- itda-front/src/components/common/CommonStyles.tsx
+++ itda-front/src/components/common/CommonStyles.tsx
@@ -1,10 +1,44 @@
 import styled from "styled-components";
 
 const S = {
-	Header: {
-		leftDiv: styled.div``,
-	},
-	ProductCard: {},
+  Header: {
+    leftDiv: styled.div``,
+  },
+  ProductCard: {
+    CardContainer: styled.div`
+      display: flex;
+      flex-direction: column;
+      width: 150px;
+      height: 170px;
+      border: 1px solid black;
+    `,
+
+    ProductImageHolder: styled.div`
+      width: 150px;
+      height: 100px;
+      overflow: hidden;
+    `,
+
+    ProductImage: styled.img`
+      width: 100%;
+      height: 100%;
+      &:hover {
+        transform: scale(1.05);
+        transition: 0.2s ease-in-out;
+      }
+    `,
+
+    ProductDescription: styled.div`
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    `,
+
+    ProductDescriptionTop: styled.div`
+      font-size: 15px;
+      height: 40px;
+    `,
+  },
 };
 
 export default S;
--- itda-front/src/components/common/ProductCard.tsx
+++ itda-front/src/components/common/ProductCard.tsx
@@ -1,6 +1,22 @@
 import S from "./CommonStyles";
 const ProductCard = () => {
-	return <div></div>;
+  return (
+    <S.ProductCard.CardContainer>
+      <S.ProductCard.ProductImageHolder>
+        <S.ProductCard.ProductImage
+          alt="이미지"
+          src="https://t1.daumcdn.net/news/202105/25/catlab/20210525054449077awum.jpg"
+        />
+      </S.ProductCard.ProductImageHolder>
+      <S.ProductCard.ProductDescription>
+        <S.ProductCard.ProductDescriptionTop>
+          <span>[크롱]</span>
+          <span>크롱의 싱싱한 당근 2kg</span>
+        </S.ProductCard.ProductDescriptionTop>
+        <div>3000원</div>
+      </S.ProductCard.ProductDescription>
+    </S.ProductCard.CardContainer>
+  );
 };
 
 export default ProductCard;
