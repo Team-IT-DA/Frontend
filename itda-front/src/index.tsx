69dd19dcc66f63aebca8dc2fa7be10302e6bbe0f
Jungwon Lee <leejungwon96@gmail.com>
2021-07-30 22:26:55 +0900

    chore: index.tsx에 StylesProvider injectFirst 적용

--- itda-front/src/index.tsx
+++ itda-front/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
 import GlobalStyle from "styles/GlobalStyles";
+import { StylesProvider } from "@material-ui/core/styles";
 
 import App from "./App";
 
 ReactDOM.render(
-	<ThemeProvider theme={theme}>
-		<GlobalStyle />
-		<App />
-	</ThemeProvider>,
-	document.getElementById("root")
+  <StylesProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </StylesProvider>,
+  document.getElementById("root")
 );
