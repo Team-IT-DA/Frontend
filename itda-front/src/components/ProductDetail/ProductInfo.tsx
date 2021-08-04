7bf49900ddf4c34bc6ae4afac5733dcc699abc74
Jungwon Lee <leejungwon96@gmail.com>
2021-08-04 10:07:39 +0900

    feat: 제품 설명 부분 요소들 배치

--- itda-front/src/components/ProductDetail/ProductInfo.tsx
+++ itda-front/src/components/ProductDetail/ProductInfo.tsx
@@ -3,15 +3,41 @@ const ProductInfo = () => {
     <>
       <div>
         <div>
-          <img alt="상품 이미지" />
+          <img
+            src="https://freshleader.co.kr/data/item/1f8_1060_0008/thumb-1f8_1_450x520.jpg"
+            alt="상품 이미지"
+          />
         </div>
         <div>
-          <div>상품 설명</div>
+          <div>크롱의 흙당근 2kg</div>
+          <div>제주 바다 바람을 품은 친환경 흙당근</div>
+          <div>19,900원</div>
+          <div>
+            <div>판매 단위 1봉지</div>
+            <div>중량/용량 500g(2~4개입)</div>
+            <div>배송 2,500원 (30,000원 이상 무료 배송)</div>
+            <div>원산지 제주</div>
+            <div>포장타입 냉장/종이포장</div>
+            <div>안내사항 -식품 특성상 중량은 5%내외의 차이가 있습니다.</div>
+          </div>
+          <div>
+            <div>수량조절버튼</div>
+            <div>총 상품 금액:19,900</div>
+          </div>
         </div>
       </div>
       <div>
-        <div>크롱연락</div>
-        <div>장바구니 버튼2개부분</div>
+        <div>
+          <img alt="판매자사진" />
+          <div>
+            <div>크롱에게 문의하기</div>
+            <div>신선한 제품을 재배합니다</div>
+          </div>
+        </div>
+        <div>
+          <button>장바구니 담기</button>
+          <button>구매하기</button>
+        </div>
       </div>
     </>
   );
