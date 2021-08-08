16245af53d07f8ddde5e11dd068a20b83e16f418
Dami Kim <dami.k.dev@gmail.com>
2021-08-08 12:21:28 +0900

    feat: Product card mock data 추가 및 적용 (#37)

--- itda-front/src/components/Products/ProductList.tsx
+++ itda-front/src/components/Products/ProductList.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import S from "./ProductsStyles";
+import ProductCard from "components/common/ProductCard";
+import productPage from "util/mock/productData";
 
 const ProductList = () => {
-	return (
-		<div>
-			<div>감자</div>
-			<div>감자</div>
-			<div>감자</div>
-		</div>
-	);
+  const { products } = productPage.data;
+  const productList = products.map(
+    ({ id, imageUrl, productName, sellerName, price }) => (
+      <S.ProductList.Block>
+        <ProductCard
+          key={id}
+          size="extra"
+          horizontal={false}
+          productImg={imageUrl}
+          productName={productName}
+          productPrice={price}
+          seller={sellerName}
+        />
+      </S.ProductList.Block>
+    )
+  );
+
+  return (
+    <S.ProductList.Layout>
+      <S.ProductList.CountLayer>'크롱' 검색 결과 3건</S.ProductList.CountLayer>
+      <S.ProductList.Layer>{productList}</S.ProductList.Layer>
+    </S.ProductList.Layout>
+  );
 };
 
 export default ProductList;
---
+++ itda-front/src/util/mock/productData.ts
@@ -0,0 +1,153 @@
+const productPage = {
+  success: true,
+  data: {
+    products: [
+      {
+        id: 1,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 2,
+        imageUrl:
+          "https://pds.joins.com/news/component/htmlphoto_mmdata/201906/26/a9da5be9-d08e-42a3-8943-188086d2b844.jpg",
+        productName: "엽떡 짝꿍",
+        sellerName: "이크롱",
+        price: 50000,
+      },
+      {
+        id: 3,
+        imageUrl:
+          "https://pds.joins.com/news/component/htmlphoto_mmdata/201906/26/a9da5be9-d08e-42a3-8943-188086d2b844.jpg",
+        productName: "엽떡 짝꿍",
+        sellerName: "김크롱",
+        price: 50000,
+      },
+      {
+        id: 4,
+        imageUrl:
+          "https://pds.joins.com/news/component/htmlphoto_mmdata/201906/26/a9da5be9-d08e-42a3-8943-188086d2b844.jpg",
+        productName: "엽떡 짝꿍",
+        sellerName: "최크롱",
+        price: 50000,
+      },
+      {
+        id: 5,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 6,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 7,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+
+      {
+        id: 8,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 9,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 10,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 11,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 12,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 13,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 14,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 15,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 16,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 17,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 18,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 19,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+      {
+        id: 20,
+        imageUrl: "https://www.yupdduk.com/images/menu/2020_hotmenuY01.png",
+        productName: "맛있는 엽떡",
+        sellerName: "박크롱",
+        price: 50000,
+      },
+    ],
+  },
+  error: null,
+};
+export default productPage;
