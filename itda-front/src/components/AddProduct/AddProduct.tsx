583f872715dead5784db29d5b8cc957ad53fe169
Jungwon Lee <leejungwon96@gmail.com>
2021-08-14 23:05:57 +0900

    feat: Material Ui 사용하여 textfield 배치, style 일부 적용

--- itda-front/src/components/AddProduct/AddProduct.tsx
+++ itda-front/src/components/AddProduct/AddProduct.tsx
@@ -1,51 +1,120 @@
+import { TextField } from "@material-ui/core";
+import S from "./AddProductStyles";
+
 const AddProduct = () => {
   return (
-    <div>
+    <S.AddProductLayout>
       <h1>상품추가</h1>
-      <div>
-        <div>
-          이미지영역
+      <S.AddProductFormLayer>
+        <S.AddProductImageBlock>
+          <S.AddProductImageHolder>
+            <S.AddProductImg src="" alt="상품 이미지" />
+          </S.AddProductImageHolder>
           <input type="file" />
-        </div>
-        <div>
+        </S.AddProductImageBlock>
+        <S.AddProductFormBlock>
           <div>
-            상품명
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-name"
+              error={false}
+              label="상품명"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="name"
+            />
           </div>
           <div>
-            상품 설명
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="상품 설명"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            상품 가격
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="상품 가격"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            판매 단위
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="판매 단위"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            배송비
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="배송비"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            배송비 추가 조건
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="배송비 추가 조건"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            중량/용량
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="중량/용량"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
           <div>
-            안내사항
-            <input />
+            <TextField
+              InputLabelProps={{ shrink: true }}
+              id="outlined-textarea"
+              error={false}
+              label="안내 사항"
+              type="text"
+              variant="outlined"
+              size="small"
+              name="description"
+            />
           </div>
-        </div>
-      </div>
-      <div>
+        </S.AddProductFormBlock>
+      </S.AddProductFormLayer>
+      <S.AddProductEditorLayer>
         <h1>에디터 영역</h1>
-      </div>
-    </div>
+      </S.AddProductEditorLayer>
+    </S.AddProductLayout>
   );
 };
 
--- itda-front/src/components/AddProduct/AddProductStyles.tsx
+++ itda-front/src/components/AddProduct/AddProductStyles.tsx
@@ -1,5 +1,49 @@
-const AddProductStyles = () => {
-	return <div></div>;
+import styled from "styled-components";
+import { TextField } from "@material-ui/core";
+
+const S = {
+  AddProductLayout: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 auto;
+    width: 1080px;
+    border: 1px solid black;
+  `,
+
+  AddProductFormLayer: styled.div`
+    display: flex;
+    margin: 0 auto;
+  `,
+  AddProductImageBlock: styled.div``,
+
+  AddProductImageHolder: styled.div`
+    border: 1px solid green;
+    width: 250px;
+    height: 300px;
+  `,
+
+  AddProductImg: styled.img`
+    width: 100%;
+    height: 100%;
+  `,
+
+  AddProductFormBlock: styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    height: fit-content;
+    grid-gap: 2rem;
+    div {
+      display: flex;
+      flex-direction: column;
+    }
+    input {
+      width: 300px;
+    }
+  `,
+
+  AddProductEditorLayer: styled.div``,
 };
 
-export default AddProductStyles;
+export default S;
