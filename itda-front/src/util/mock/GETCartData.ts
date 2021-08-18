459378f4f54962c6c4fb714b86c681cc42ba1570
Dami Kim <dami.k.dev@gmail.com>
2021-08-18 20:46:08 +0900

    chore: test 용 cart mock data 추가(#86)

---
+++ itda-front/src/util/mock/GETCartData.ts
@@ -0,0 +1,23 @@
+export const GETCartData = {
+  success: true,
+  data: {
+    defaultAddress: "서울특별시 강남구 역삼동",
+    detail: [
+      {
+        id: 1,
+        imageUrl: "https://i.ndtvimg.com/mt/cooks/2014-11/carrots.jpg",
+        productName: "제품 이름",
+        price: 50000,
+        count: 2,
+      },
+      {
+        id: 2,
+        imageUrl: "https://i.ndtvimg.com/mt/cooks/2014-11/carrots.jpg",
+        productName: "제품 이름",
+        price: 50000,
+        count: 6,
+      },
+    ],
+  },
+  error: null,
+};
