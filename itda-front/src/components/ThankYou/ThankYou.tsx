53c92be9b3fa99c726cfd984372a288259d2b5f8
Jungwon Lee <leejungwon96@gmail.com>
2021-09-04 19:37:33 +0900

    feat: thankyou 페이지 뷰 완성

--- itda-front/src/components/ThankYou/ThankYou.tsx
+++ itda-front/src/components/ThankYou/ThankYou.tsx
@@ -5,23 +5,47 @@ const ThankYou = () => {
   return (
     <div>
       <Header />
-      <div>
+      <S.ThankyouLayout>
         <h1>마음을 잇는 현명한 소비에 함께해주셔서 감사합니다.</h1>
         <h3>24시간 이내로 무통장입금을 완료하지 않으시면 자동취소 됩니다.</h3>
-        <div>
-          <div>주문 내역</div>
-          <div>
-            <div>
+        <S.OrderLayerTitle>주문 내역</S.OrderLayerTitle>
+        <S.OrderLayer>
+          <S.OrderItemBlock>
+            <S.OrderItemImg
+              src="https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/21/1/1/0/2/2/9/LdeGY/3721110229_B.jpg"
+              alt="당근"
+            />
+            <S.OrderItemDescription>
+              <S.OrderItemTitle>김크롱의 유기농 당근 2kg</S.OrderItemTitle>
               <div>
-                <img src="" alt="당근" />
+                <span>수량: 1</span>
+                <span>20,000원</span>
               </div>
-              김크롱의 유기농 당근 2kg
-            </div>
-          </div>
-        </div>
-        <div>
-          <div>결제 내역</div>
-          <div>
+            </S.OrderItemDescription>
+          </S.OrderItemBlock>
+          <S.OrderItemBlock>
+            <S.OrderItemImg
+              src="https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/21/1/1/0/2/2/9/LdeGY/3721110229_B.jpg"
+              alt="당근"
+            />
+            <S.OrderItemDescription>
+              <S.OrderItemTitle>김크롱의 유기농 당근 2kg</S.OrderItemTitle>
+              <div>
+                <span>수량: 1</span>
+                <span>20,000원</span>
+              </div>
+            </S.OrderItemDescription>
+          </S.OrderItemBlock>
+          <S.OrderItemTotalPrice>
+            <dl>
+              <dt>총 주문 금액</dt>
+              <dd>20,000원</dd>
+            </dl>
+          </S.OrderItemTotalPrice>
+        </S.OrderLayer>
+        <S.PaymentLayerTitle>결제 내역</S.PaymentLayerTitle>
+        <S.PaymentLayer>
+          <S.PaymentItemBlock>
             <dl>
               <dt>결제 수단</dt>
               <dd>무통장 입금</dd>
@@ -42,10 +66,10 @@ const ThankYou = () => {
               <dt>입금 금액</dt>
               <dd>22,500원</dd>
             </dl>
-          </div>
+          </S.PaymentItemBlock>
           <div></div>
-        </div>
-      </div>
+        </S.PaymentLayer>
+      </S.ThankyouLayout>
     </div>
   );
 };
--- itda-front/src/components/ThankYou/ThankyouStyles.ts
+++ itda-front/src/components/ThankYou/ThankyouStyles.ts
@@ -1,8 +1,94 @@
 import styled from "styled-components";
 
 const S = {
-  Layout: styled.div`
+  ThankyouLayout: styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 1050px;
+    margin: 3rem auto;
+    h3 {
+      font-weight: normal;
+      color: ${({ theme }) => theme.colors.gray.normal};
+    }
+  `,
+
+  ThankyouMessageLayer: styled.div``,
+
+  OrderLayer: styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid ${({ theme }) => theme.colors.gray.x_light};
+    width: 100%;
+    padding: 1rem;
+    font-weight: bold;
+  `,
+
+  OrderLayerTitle: styled.div`
+    margin: 0.8rem 0;
+    font-size: ${({ theme }) => theme.fontSizes.xl};
+    font-weight: bold;
+  `,
+
+  OrderItemBlock: styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+  `,
+
+  OrderItemImg: styled.img`
+    width: 200px;
+    height: 150px;
+    margin-right: 3rem;
+  `,
+
+  OrderItemDescription: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1rem 0;
+    span {
+      display: block;
+    }
+  `,
+
+  OrderItemTitle: styled.span`
+    font-weight: bold;
+    font-size: ${({ theme }) => theme.fontSizes.l};
+  `,
+
+  OrderItemTotalPrice: styled.div`
+    display: flex;
+    justify-content: flex-end;
+    font-weight: bold;
+  `,
+
+  PaymentLayer: styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid ${({ theme }) => theme.colors.gray.x_light};
+    border-bottom: none;
+    width: 100%;
+    font-weight: bold;
+    dl {
+      margin: 0.3rem;
+    }
+
+    dt {
+      width: 100px;
+    }
+  `,
+
+  PaymentLayerTitle: styled.div`
+    margin: 0.8rem 0;
+    font-size: ${({ theme }) => theme.fontSizes.xl};
+    font-weight: bold;
+  `,
+
+  PaymentItemBlock: styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray.x_light};
+    padding: 1rem;
   `,
 };
 
