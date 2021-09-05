7d40f29fc09a456fcf446169ce47cc67471ca973
Dami Kim <dami.k.dev@gmail.com>
2021-09-05 18:14:47 +0900

    fix: CancelButton Atom hoverEffect prop 추가 (#128)

--- itda-front/src/components/common/Atoms/CancelButton.tsx
+++ itda-front/src/components/common/Atoms/CancelButton.tsx
@@ -2,10 +2,11 @@ import S from "./AtomsStyles";
 
 type TCheckButton = {
   onClick?: React.MouseEventHandler<SVGElement>;
+  hoverEffect?: boolean;
 };
 
-const CancelButton = ({ onClick }: TCheckButton) => {
-  return <S.CancelButton.Icon onClick={onClick} />;
+const CancelButton = ({ onClick, hoverEffect = true }: TCheckButton) => {
+  return <S.CancelButton.Icon onClick={onClick} hoverEffect={hoverEffect} />;
 };
 
 export default CancelButton;
