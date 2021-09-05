0c51dfc0b570164ad278862e7e80c6cf0a95c89b
Dami Kim <dami.k.dev@gmail.com>
2021-09-05 18:20:56 +0900

    style: Cart Page 내 Layout 변경 (#110)

--- itda-front/src/components/Cart/Cart.tsx
+++ itda-front/src/components/Cart/Cart.tsx
@@ -3,13 +3,18 @@ import Header from "components/common/Header";
 import AddressInfo from "./AddressInfo";
 import PaymentInfo from "./PaymentInfo";
 import CartProduct from "./CartProduct/";
-
+import AddressForm from "./AddressForm";
 const Cart = () => {
   return (
     <>
+      <S.AddressFormModal.BlackBackground />
+      {/* 모달 클릭 여부에 따라 리턴 */}
       <S.Cart.HeaderLayout>
         <Header />
       </S.Cart.HeaderLayout>
+      <S.Cart.ModalLayout>
+        <AddressForm />
+      </S.Cart.ModalLayout>
       <S.Cart.CartHeaderLayout>장바구니</S.Cart.CartHeaderLayout>
       <S.Cart.MainLayout>
         <S.Cart.ContainerLayer>
