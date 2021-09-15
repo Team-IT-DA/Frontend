7183514a3d69568d41cb95f4ad151e556079bd27
Hannah Park <ha3158987@gmail.com>
2021-09-15 18:25:32 +0900

    feat: 하위탭 스타일 구현 (#154)

--- itda-front/src/components/MyPage/MyPageStyles.ts
+++ itda-front/src/components/MyPage/MyPageStyles.ts
@@ -56,7 +56,11 @@ const S = {
   },
 
   MyPageTab: {
-    Layout: styled.div<{
+    Layout: styled.div`
+      position: relative;
+    `,
+
+    TabLayer: styled.div<{
       category: string;
       currentSelectedTab: string;
     }>`
@@ -81,6 +85,27 @@ const S = {
         color: ${({ theme }) => theme.colors.navy.normal};
         transition: color 0.3s;
       }
+    `,
+
+    SubtabLayer: styled.div`
+      position: absolute;
+      left: 270px;
+      top: 0;
+      width: 130px;
+      cursor: pointer;
+    `,
+  },
+
+  SellerSubtabs: {
+    Layout: styled.ul`
+      border: 1px solid ${({ theme }) => theme.colors.gray.light};
+      border-radius: 15px;
+    `,
+
+    Subtab: styled.li`
+      text-align: center;
+      border: 1px solid red;
+      padding: 1rem;
     `,
   },
 
--- itda-front/src/components/MyPage/MyPageTab/SellerSubtabs.tsx
+++ itda-front/src/components/MyPage/MyPageTab/SellerSubtabs.tsx
@@ -1,5 +1,14 @@
+import S from "../MyPageStyles";
+
 const SellerSubtabs = () => {
-  return <div></div>;
+  const subtabTitles = ["기본정보", "판매자 정보"];
+  return (
+    <S.SellerSubtabs.Layout>
+      {subtabTitles.map((subtab) => (
+        <S.SellerSubtabs.Subtab>{subtab}</S.SellerSubtabs.Subtab>
+      ))}
+    </S.SellerSubtabs.Layout>
+  );
 };
 
 export default SellerSubtabs;
