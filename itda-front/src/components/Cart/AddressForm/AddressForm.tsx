ba895f407cdff02403c3371769e45174777eb807
Dami Kim <dami.k.dev@gmail.com>
2021-09-15 16:58:15 +0900

    feat: Cart AddressFormModal 토글 기능 구현 (#145)

--- itda-front/src/components/Cart/AddressForm/AddressForm.tsx
+++ itda-front/src/components/Cart/AddressForm/AddressForm.tsx
@@ -1,6 +1,10 @@
 import AddressFormModal from "./AddressFormModal";
-const AddressForm = () => {
-  return <>{<AddressFormModal />}</>;
+const AddressForm = ({
+  toggleAddressForm,
+}: {
+  toggleAddressForm: React.MouseEventHandler<SVGElement>;
+}) => {
+  return <>{<AddressFormModal toggleAddressForm={toggleAddressForm} />}</>;
 };
 
 export default AddressForm;
