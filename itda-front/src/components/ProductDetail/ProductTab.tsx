5e0e209c7b93ac0f57d11b243e934d237f377454
Jungwon Lee <leejungwon96@gmail.com>
2021-09-21 12:59:54 +0900

    refactor: 함수명 변경 및 기능별 새로운 함수로 묶기(#156)

--- itda-front/src/components/ProductDetail/ProductTab.tsx
+++ itda-front/src/components/ProductDetail/ProductTab.tsx
@@ -17,16 +17,20 @@ const ProductTab = () => {
   const reviewRef = useRef<HTMLDivElement>(null);
   const infoRef = useRef<HTMLDivElement>(null);
 
+  const setTabSticky = (checkpoint: IntersectionObserverEntry) => {
+    if (checkpoint.boundingClientRect.y <= 0) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
   useEffect(() => {
     const checkLine = tabRef?.current;
     const io = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (entry.boundingClientRect.y <= 0) {
-            setSticky(true);
-          } else {
-            setSticky(false);
-          }
+          setTabSticky(entry);
         });
       },
       { threshold: [1] }
@@ -35,12 +39,12 @@ const ProductTab = () => {
     io.observe(checkLine as HTMLDivElement);
   }, [tabRef]);
 
-  const handleInfoClick = () => {
+  const moveToInfoSection = () => {
     setTabStateInfo(true);
     infoRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
-  const handleReviewClick = () => {
+  const moveToReviewSection = () => {
     setTabStateInfo(false);
     reviewRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
@@ -51,13 +55,13 @@ const ProductTab = () => {
       <S.ProductTab.TabToggleLayer isSticky={isSticky}>
         <S.ProductTab.InformationTabBlock
           isInfo={isTabStateInfo}
-          onClick={handleInfoClick}
+          onClick={moveToInfoSection}
         >
           상품설명
         </S.ProductTab.InformationTabBlock>
         <S.ProductTab.ReviewTabBlock
           isInfo={isTabStateInfo}
-          onClick={handleReviewClick}
+          onClick={moveToReviewSection}
         >
           후기
         </S.ProductTab.ReviewTabBlock>
