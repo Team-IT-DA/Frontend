68888be2dd0edd8ef61d087a80bcd5bae38601de
Hannah Park <ha3158987@gmail.com>
2021-09-29 19:00:04 +0900

    fix: 주문내역 컴포넌트 레이아웃 수정 (#170)

--- itda-front/src/components/MyPage/MyPageOrderList/MyPageOrderList.tsx
+++ itda-front/src/components/MyPage/MyPageOrderList/MyPageOrderList.tsx
@@ -6,9 +6,12 @@ import SelectBox from "components/common/Atoms/SelectBox";
 import ReceiptModal from "./ReceiptModal";
 import { useState } from "react";
 import MyOrderList from "./MyOrderList";
+import Header from "components/common/Header";
+import MyPageTabs from "../MyPageTab/MyPageTabs";
 import S from "../MyPageStyles";
 
 const MyPageOrderList = () => {
+  const [isSeller, setIsSeller] = useState(true);
   const [isReviewModalOpen, toggleReviewModal] = useToggle(false);
   const [isOrderDetailModalOpen, toggleOrderDetailModal] = useToggle(false);
   const [page, setPage] = useState(1);
@@ -17,7 +20,6 @@ const MyPageOrderList = () => {
     if (target.closest(".reviewModal")) return;
     toggleReviewModal();
   };
-
 
   const orderDetailHandler = (e: React.MouseEvent<HTMLElement>) => {
     const target = e.target as HTMLElement;
@@ -28,31 +30,45 @@ const MyPageOrderList = () => {
   const dateFilter = ["전체", "최근 3개월", "최근 6개월", "최근 1년"];
   const { orderList } = newOrderListData;
   return (
-    <S.MyOrderList.Layout>
-      <SelectBox selectArray={dateFilter} />
-      <MyOrderList
-        orderList={orderList}
-        width={"75%"}
-        onClickReviewButton={reviewModalHandler}
-        onClickOrderDetailButton={orderDetailHandler}
-      />
-      {isReviewModalOpen && (
-        <ReviewWriteForm handleModalOpen={reviewModalHandler} />
-      )}
-      {isOrderDetailModalOpen && (
-        <ReceiptModal
-          orderList={orderList}
-          handleModalOpen={orderDetailHandler}
-          toggleHandler={toggleOrderDetailModal}
-        />
-      )}
-      <Pagination
-        itemsPerPage={orderPerPage}
-        totalPosts={20}
-        paginate={setPage}
-        currentPage={page}
-      />
-    </S.MyOrderList.Layout>
+    <>
+      <S.MyPage.HeaderLayout>
+        <Header />
+      </S.MyPage.HeaderLayout>
+      <S.MyPage.MainLayout>
+        <S.MyPage.SideTabLayout>
+          <MyPageTabs isSeller={isSeller} />
+        </S.MyPage.SideTabLayout>
+        <S.MyPage.ContentLayout>
+          <S.MyPage.ContentLayer>
+            <S.MyOrderList.Layout>
+              <SelectBox selectArray={dateFilter} />
+              <MyOrderList
+                orderList={orderList}
+                width={"75%"}
+                onClickReviewButton={reviewModalHandler}
+                onClickOrderDetailButton={orderDetailHandler}
+              />
+              {isReviewModalOpen && (
+                <ReviewWriteForm handleModalOpen={reviewModalHandler} />
+              )}
+              {isOrderDetailModalOpen && (
+                <ReceiptModal
+                  orderList={orderList}
+                  handleModalOpen={orderDetailHandler}
+                  toggleHandler={toggleOrderDetailModal}
+                />
+              )}
+              <Pagination
+                itemsPerPage={orderPerPage}
+                totalPosts={20}
+                paginate={setPage}
+                currentPage={page}
+              />
+            </S.MyOrderList.Layout>
+          </S.MyPage.ContentLayer>
+        </S.MyPage.ContentLayout>
+      </S.MyPage.MainLayout>
+    </>
   );
 };
 
