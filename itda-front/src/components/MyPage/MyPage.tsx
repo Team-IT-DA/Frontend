a0a610b03a6a159bffad5726833c38e5d0a64453
Hannah Park <ha3158987@gmail.com>
2021-09-29 20:30:34 +0900

    fix: 불필요한 상태 삭제 (#170)

--- itda-front/src/components/MyPage/MyPage.tsx
+++ itda-front/src/components/MyPage/MyPage.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
 import { Route, Switch } from "react-router-dom";
 import S from "./MyPageStyles";
 import MyReview from "components/MyPage/MyPageReview/MyReview";
 import MyPageOrderList from "./MyPageOrderList/MyPageOrderList";
 import MyInfoEditBefore from "./MyPageInfo/MyInfoEditBefore";
-import { currentSelectedTab, currentSelectedSubtab } from "stores/MyPageAtoms";
-import { isLoggedIn } from "stores/LoginAtoms";
 
 const MyPage = () => {
-  const currentSelectedTabState = useRecoilValue(currentSelectedTab);
-  const currentSelectedSubtabState = useRecoilValue(currentSelectedSubtab);
-  //임시로 만든 로그인 상태: true => 기본정보수정 페이지, 판매자 페이지 보여짐
-  const isLoggedInState = useRecoilValue(isLoggedIn);
-  //임시로 만든 판매자 식별 상태 => 판매자용 화면/탭 보기: true
-  const [isSeller, setIsSeller] = useState(true);
-
   return (
     <>
       <S.MyPage.Layout>
