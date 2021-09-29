b7e677e49ca76dbac71c1a1cff354b320f9461a1
Hannah Park <ha3158987@gmail.com>
2021-09-29 20:23:37 +0900

    fix: MyPage 안쓰이는 코드 삭제 (#170)

--- itda-front/src/components/MyPage/MyPage.tsx
+++ itda-front/src/components/MyPage/MyPage.tsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { Link, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import S from "./MyPageStyles";
-import Header from "components/common/Header";
-import MyPageTabs from "components/MyPage/MyPageTab/MyPageTabs";
 import MyReview from "components/MyPage/MyPageReview/MyReview";
 import MyPageOrderList from "./MyPageOrderList/MyPageOrderList";
 import MyInfoEditBefore from "./MyPageInfo/MyInfoEditBefore";
-import MyInfoEditAfter from "./MyPageInfo/MyInfoEditAfter";
-import { SellerInfoEdit } from "./SellerPage";
 import { currentSelectedTab, currentSelectedSubtab } from "stores/MyPageAtoms";
 import { isLoggedIn } from "stores/LoginAtoms";
 
@@ -23,36 +19,13 @@ const MyPage = () => {
   return (
     <>
       <S.MyPage.Layout>
-        {/* <S.MyPage.HeaderLayout>
-          <Header />
-        </S.MyPage.HeaderLayout>
-        <S.MyPage.MainLayout>
-          <S.MyPage.SideTabLayout>
-            <MyPageTabs isSeller={isSeller} />
-          </S.MyPage.SideTabLayout>
-          <S.MyPage.ContentLayout>
-            <S.MyPage.ContentLayer>
-              {currentSelectedTabState === "주문 내역" && <MyPageOrderList />}
-              {currentSelectedTabState === "상품 후기" && <MyReview />}
-              {currentSelectedTabState === "잇다톡"}
-              {currentSelectedTabState === "개인 정보 수정" &&
-                (!isLoggedInState ? (
-                  <MyInfoEditBefore />
-                ) : currentSelectedSubtabState === "기본정보" ? (
-                  <MyInfoEditAfter />
-                ) : (
-                  <SellerInfoEdit />
-                ))}
-              {currentSelectedTabState === "등록 상품 조회" && (
-                <>등록된 상품 목록 보여주기</>
-              )}
-            </S.MyPage.ContentLayer>
-          </S.MyPage.ContentLayout>
-        </S.MyPage.MainLayout> */}
-        <Route path="/myPage/orderList" component={MyPageOrderList} />
-        <Route path="/myPage/reviews" component={MyReview} />
-        <Route path="/myPage/itdaTalk" />
-        <Route path="/myPage/myInfoEdit" component={MyInfoEditBefore} />
+        <Switch>
+          <Route exact path="/myPage" component={MyPageOrderList} />
+          <Route path="/myPage/orderList" component={MyPageOrderList} />
+          <Route path="/myPage/reviews" component={MyReview} />
+          <Route path="/myPage/itdaTalk" />
+          <Route path="/myPage/myInfoEdit" component={MyInfoEditBefore} />
+        </Switch>
       </S.MyPage.Layout>
     </>
   );
