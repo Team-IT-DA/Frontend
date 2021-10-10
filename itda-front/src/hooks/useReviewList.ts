3d4c484179e5e7bd34d650343eecc8d8328f4e8b
Jungwon Lee <leejungwon96@gmail.com>
2021-10-10 22:56:32 +0900

    feat: useReviewList 커스텀 hook 구현해보기(#192)

---
+++ itda-front/src/hooks/useReviewList.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { useQuery } from "react-query";
+import { isReviewOnlyPhoto, productInfo } from "stores/ProductDetailAtoms";
+import { productAPI } from "util/API/productAPI";
+
+export function useReviewList() {
+  const [page, setPage] = useState(1);
+  const isPhotoReview = useRecoilValue(isReviewOnlyPhoto);
+  const reviewsPerPage = 5;
+  const productData = useRecoilValue(productInfo);
+  const { isLoading, error, isError, data } = useQuery(
+    ["reviewList"],
+    () =>
+      productAPI.products.get.getProductReview(
+        productData.id,
+        page,
+        isPhotoReview,
+        reviewsPerPage
+      ),
+    {
+      retry: 1,
+    }
+  );
+  console.log(productData.id);
+
+  return {
+    data,
+    isLoading,
+    error,
+    isError,
+    page,
+    setPage,
+    reviewsPerPage,
+  };
+}
