1738e1dec9816e33cf6eb81cdcf099fab9e7e728
Dami Kim <dami.k.dev@gmail.com>
2021-10-10 14:58:03 +0900

    feat: CategoryList API data로 구현 (#188)

--- itda-front/src/components/Products/Navigator/CategoryList.tsx
+++ itda-front/src/components/Products/Navigator/CategoryList.tsx
@@ -2,51 +2,46 @@ import S from "../ProductsStyles";
 import { Redirect } from "react-router-dom";
 import { useState } from "react";
 import useToggle from "hooks/useToggle";
+import CategoryService from "./CategoryService";
+
 const CategoryList = () => {
-	const [toggleFlag, setToggleFlag] = useToggle();
-	const [selectedCategory, setSelectedCategory] = useState("");
-	const handleCategoryListClick = (event: React.MouseEvent) => {
-		const target = event.target as HTMLElement;
-		setToggleFlag();
-		setSelectedCategory(target.textContent as string);
-	};
+  const [toggleFlag, setToggleFlag] = useToggle();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const { categoryData } = CategoryService();
 
-	const list = [
-		"채소",
-		"과일/견과/쌀",
-		"수산/해산/건어물",
-		"정육/계란",
-		"면/양념/오일",
-		"음료/우유/커피/차",
-		"베이커리/치즈/델리",
-		"간식/과자/떡",
-	].map((v, i) => (
-		<S.CategoryList.List
-			key={`category-${i}`}
-			onClick={handleCategoryListClick}
-		>
-			{v}
-		</S.CategoryList.List>
-	));
+  const handleCategoryListClick = (event: React.MouseEvent) => {
+    const target = event.target as HTMLElement;
+    setToggleFlag();
+    setSelectedCategory(target.textContent as string);
+  };
 
-	return (
-		<>
-			{selectedCategory && (
-				<Redirect to={`/products?category=${selectedCategory}`} />
-			)}
-			<S.CategoryList.Layer>
-				<S.CategoryList.ButtonLayout>
-					<S.CategoryList.MenuButton onMouseEnter={setToggleFlag} />
-					<S.CategoryList.Text>전체 카테고리</S.CategoryList.Text>
-				</S.CategoryList.ButtonLayout>
-				{toggleFlag && (
-					<S.CategoryList.ListLayout onMouseLeave={setToggleFlag}>
-						{list}
-					</S.CategoryList.ListLayout>
-				)}
-			</S.CategoryList.Layer>
-		</>
-	);
+  const categoryList = categoryData?.map((category: string, idx: number) => (
+    <S.CategoryList.List
+      key={`category-${idx}`}
+      onClick={handleCategoryListClick}
+    >
+      {category}
+    </S.CategoryList.List>
+  ));
+
+  return (
+    <>
+      {selectedCategory && (
+        <Redirect to={`/products?category=${selectedCategory}`} />
+      )}
+      <S.CategoryList.Layer>
+        <S.CategoryList.ButtonLayout>
+          <S.CategoryList.MenuButton onMouseEnter={setToggleFlag} />
+          <S.CategoryList.Text>전체 카테고리</S.CategoryList.Text>
+        </S.CategoryList.ButtonLayout>
+        {toggleFlag && (
+          <S.CategoryList.ListLayout onMouseLeave={setToggleFlag}>
+            {categoryList}
+          </S.CategoryList.ListLayout>
+        )}
+      </S.CategoryList.Layer>
+    </>
+  );
 };
 
 export default CategoryList;
