72615da9f776606971f0fe3e02635bdbcc14348f
Dami Kim <dami.k.dev@gmail.com>
2021-10-13 00:53:36 +0900

    feat: POST order, GET shippingInfos util 추가 (#193)

--- itda-front/src/util/API/cartAPI.ts
+++ itda-front/src/util/API/cartAPI.ts
@@ -1,13 +1,23 @@
 import { instanceWithAuth } from "./index";
-import { IShippingInfos } from "types/CartTypes";
+import { IShippingInfos, IOrder } from "types/CartTypes";
 
 const postShippingInfos = async (shippingInfos: IShippingInfos) => {
   instanceWithAuth.post("/shippingInfos", shippingInfos);
 };
 
+const getShippingInfos = () => instanceWithAuth.get("/shippingInfos");
+
+const postOrder = async (orderList: IOrder) => {
+  instanceWithAuth.post("/shippingInfos", orderList);
+};
+
 const cartAPI = {
   shipping: {
     post: { postShippingInfos },
+    get: { getShippingInfos },
+  },
+  order: {
+    get: { postOrder },
   },
 };
 
