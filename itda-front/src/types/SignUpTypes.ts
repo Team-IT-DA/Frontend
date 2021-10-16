50a3e15ad1d1fde346178ed0dab9de9718c50cb2
Jungwon Lee <leejungwon96@gmail.com>
2021-10-16 19:33:03 +0900

    chore: signup, validation type 지정(#202)

---
+++ itda-front/src/types/SignUpTypes.ts
@@ -0,0 +1,15 @@
+export interface IValidation {
+  username?: string;
+  email?: string;
+  password?: string;
+  password2?: string;
+  telephone?: string;
+}
+
+export interface ISignUp {
+  username: string;
+  email: string;
+  password: string;
+  password2: string;
+  telephone: number;
+}
