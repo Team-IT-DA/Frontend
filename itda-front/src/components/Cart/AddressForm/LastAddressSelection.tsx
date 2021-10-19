65cc5ec5d3a7f125310f61cc812db7dd92b2a87b
Dami Kim <dami.k.dev@gmail.com>
2021-10-20 00:48:10 +0900

    fix: radiobutton 옆 cancel button 제거 (#209)

--- itda-front/src/components/Cart/AddressForm/LastAddressSelection.tsx
+++ itda-front/src/components/Cart/AddressForm/LastAddressSelection.tsx
@@ -1,4 +1,4 @@
-import RadioButtonWithCancel from "components/common/Atoms/RadioButtonWithCancel";
+import RadioButton from "components/common/Atoms/RadioButton";
 import { useState } from "react";
 import S from "../CartStyles";
 import AddressService from "../AddressService";
@@ -6,25 +6,25 @@ import AddressService from "../AddressService";
 const LastAddressSelection = () => {
   const [state, setSelectedState] = useState("test");
   // const { defaultShippingAddress, shippingAddress } = AddressService();
-  //TODO: shippingAddress로 RadioButtonWithCancel map 돌리기
+  //TODO: shippingAddress로 RadioButton map 돌리기
   return (
     <S.LastAddressSelection.Layout>
-      <RadioButtonWithCancel
+      <RadioButton
         value={"크롱이네"}
         state={state}
         setState={setSelectedState}
       />
-      <RadioButtonWithCancel
+      <RadioButton
         value={"재롱이네"}
         state={state}
         setState={setSelectedState}
       />
-      <RadioButtonWithCancel
+      <RadioButton
         value={"메롱이네"}
         state={state}
         setState={setSelectedState}
       />
-      <RadioButtonWithCancel
+      <RadioButton
         value={"아롱이네"}
         state={state}
         setState={setSelectedState}
