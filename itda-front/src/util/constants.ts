30093836ce3c6779a1b74f8c0d0f474b0ea43b4a
Jungwon Lee <leejungwon96@gmail.com>
2021-11-03 12:08:53 +0900

    chore: 상품 가격 입력 레이블 내용 변경 (#231)

--- itda-front/src/util/constants.ts
+++ itda-front/src/util/constants.ts
@@ -1,9 +1,9 @@
 export const addProductinputNames = [
   { name: "name", label: "상품명" },
   { name: "subTitle", label: "상품 설명" },
-  { name: "price", label: "상품가격" },
+  { name: "price", label: "상품가격(원 제외 숫자만 입력 ex: 4000)" },
   { name: "salesUnit", label: "판매 단위" },
-  { name: "deliveryFee", label: "배송비" },
+  { name: "deliveryFee", label: "배송비(원 제외 숫자만 입력 ex: 4000)" },
   { name: "deliveryFeeCondition", label: "배송비 추가 조건" },
   { name: "capacity", label: "중량/용량" },
   { name: "notice", label: "안내 사항" },
