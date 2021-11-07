5b6c92dc71508b9a85e71f88e111f0ce037e6c04
Jungwon Lee <leejungwon96@gmail.com>
2021-11-07 13:14:26 +0900

    chore: category 추가 오류. 수정 필요 (#239)

--- itda-front/src/components/AddProduct/AddProduct.tsx
+++ itda-front/src/components/AddProduct/AddProduct.tsx
@@ -1,11 +1,17 @@
 import Header from "components/common/Header";
 import AddProductForm from "./AddProductForm";
+import { productAPI } from "util/API/productAPI";
 
 const AddProduct = () => {
+  const addCate = () => {
+    productAPI.category.post.postCategory("과일/견과/쌀");
+  };
+
   return (
     <>
       <Header />
       <AddProductForm />
+      <button onClick={addCate}>추가</button>
     </>
   );
 };
