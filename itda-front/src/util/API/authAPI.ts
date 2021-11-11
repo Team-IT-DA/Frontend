865c83367ccd9e67878da0e3cab6c9b338eed2b4
Jungwon Lee <leejungwon96@gmail.com>
2021-11-11 13:14:39 +0900

    feat: login api, type 준비(#245)

--- itda-front/src/util/API/authAPI.ts
+++ itda-front/src/util/API/authAPI.ts
@@ -1,8 +1,12 @@
 import { instanceWithoutAuth } from "./index";
 import { ISignUp } from "types/SignUpTypes";
+import { ILogin } from "types/LoginTypes";
 
 const signUpAsUser = (userData: ISignUp) =>
   instanceWithoutAuth.post("/join", userData);
+
+const LoginAsUser = (userData: ILogin) =>
+  instanceWithoutAuth.post("/login", userData);
 
 const verifyEmail = (email: string) =>
   instanceWithoutAuth.get(`/duplicateEmail?email=${email}`);
@@ -10,7 +14,7 @@ const verifyEmail = (email: string) =>
 const auth = {
   signUp: {
     get: { verifyEmail },
-    post: { signUpAsUser },
+    post: { signUpAsUser, LoginAsUser },
   },
 };
 
